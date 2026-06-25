e3c9a1a11337dfb0a1025a4255e2bc183f50fe8f
kito-pc16 <kitokozai.pc16@outlook.jp>
2026-06-25 11:21:20 +0000

    Pending changes exported from your codespace

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="flex gap-4 bg-amber-100 h-12 text-xl p-2">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  </footer>
+</template>
+
+<style scoped></style>
