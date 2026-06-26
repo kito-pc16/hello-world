d0f30be33a8c6bed1ed0e6b7213a4711ccd0dd5e
kpc16 <johndoe@example.com>
2026-06-26 12:33:10 +0900

    About page change icon.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import { CakeIcon } from '@heroicons/vue/16/solid';
+</script>
 <template>
-    <span class="material-symbols-outlined">thumb_up</span>
     <div class="flex flex-col gap-4 border border-indigo-400 rounded-md p-2 m-2 shadow-md w-fit">
-        <p>This Page About View.</p>
+        <div class="flex gap-2">
+            <CakeIcon class="w-6 h-6" />
+            <p>This Page About View.</p>
+        </div>
         <div class="flex items-end gap-4">
             <p class="text-xl font-bold">郵便番号 ⇒ 住所 検索</p>
             <p>ver. 0.10</p>
