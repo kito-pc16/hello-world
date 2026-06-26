ce4fbaf9d1ddca3065e0aea53bd43cf1ee5c93cc
kito-pc16 <kitokozai.pc16@outlook.jp>
2026-06-26 02:42:21 +0000

    Pending changes exported from your codespace

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import { CakeIcon } from '@heroicons/vue/24/solid'
+</script>
 <template>
-    <span class="material-symbols-outlined">thumb_up</span>
     <div class="flex flex-col gap-4 border border-indigo-400 rounded-md p-2 m-2 shadow-md w-fit">
-        <p>This Page About View.</p>
+        <div class="flex gap-2">
+            <CakeIcon class="size-6 text-blue-500" />
+            <p>This Page About View.</p>
+        </div>
         <div class="flex items-end gap-4">
             <p class="text-xl font-bold">郵便番号 ⇒ 住所 検索</p>
             <p>ver. 0.10</p>
